3c0e0ccf1255d3281b7bb27998abd489f1005d0f
greymd <yamadagrep@gmail.com>
2020-04-16 23:56:49 +0000

    [skip ci] Bump version to 4.0.0 (egison-tutorial)

--- egison-tutorial.rb
+++ egison-tutorial.rb
@@ -1,10 +1,10 @@
 class EgisonTutorial < Formula
-  VERSION = "3.9.3"
+  VERSION = "4.0.0"
   version VERSION
   desc "A tutorial program for the Egison programming language"
   homepage "https://www.egison.org/"
   url "https://github.com/egison/homebrew-egison/releases/download/#{VERSION}/egison-tutorial_darwin_x86_64_#{VERSION}.zip"
-  sha256 "bd77d86665a0e5674f0504ca7719c47b87f560605b569bd4de927e3452f5c428"
+  sha256 "9c896c1a548aa9bdf6f91be733447339a48de573b7681becf56cdb84d4806dd1"
 
   depends_on "egison"
 
